704189a4f2bac411cc10f266a10f328e3dde3b09
nahimana fulgence <funfulgence@gmail.com.com>
2023-06-14 07:51:53 -0700

    simple_delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
-
 def simple_delete(a_dictionary, key=""):
-    keys = list(a_dictionary.keys())
-
-    for my_key in keys:
-        if my_key == key:
-            del a_dictionary[my_key]
-    return a_dictionary
+    if a_dictionary.get(key) is not None:
+        del a_dictionary[key]
+    return (a_dictionary)
