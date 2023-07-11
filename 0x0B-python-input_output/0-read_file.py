5776ac0a4ff604247992caa8d002afcd199f3e89
nahimana fulgence <funfulgence@gmail.com.com>
2023-07-11 08:15:51 -0700

    pythonInputOutput

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""reads a file"""
+"""
+a function that reads a text file (UTF8) and prints it to stdout
+"""
 
 
 def read_file(filename=""):
-    """reads a file"""
-    with open(filename, mode='r', encoding='utf-8') as f:
-        for line in f:
-            print(line, end="")
+    """
+    a function that reads a text file (UTF8) and prints it to stdout
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""advanced"""
+"""
+append after a specific string module
+"""
 
 
 def append_after(filename="", search_string="", new_string=""):
-    """appends after"""
-    with open(filename, mode='r', encoding='utf-8') as f:
-        lines = f.readlines()
-
-    with open(filename, mode='w', encoding='utf-8') as f:
-        for line in lines:
-            f.write(line)
+    """
+    inserts a line of text to a file,
+    after each line containing a specific string
+    """
+    with open(filename, "r+", encoding="utf-8") as f:
+        s = ""
+        for line in f:
+            s += line
             if search_string in line:
-                f.write(new_string)
+                s += new_string
+        f.seek(0)
+        f.write(s)
