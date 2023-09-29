f7611cc7724dd427fd43d305824fcbac78a6a9d2
Fulgence Nahimana <funfulgence@gmail.com>
2023-09-29 07:57:13 -0700

    python-network_1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-Gets the last 10 commits from a specific repo, with the hashsum and name.
-"""
+import sys
 import requests
-from sys import argv
 
 
 if __name__ == "__main__":
-    req = requests.get("https://api.github.com/repos/"
-                       "{}/{}/commits".
-                       format(argv[2], argv[1]))
-    for i in range(0, min(len(req.json()), 10)):
-        print("{}: {}".format(
-                req.json()[i]['sha'],
-                req.json()[i]['commit']['author']['name']
-            ))
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
-# requests use
-import requests
-from sys import argv
+"""A script that:
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
+"""
+import sys
+import urllib.request
 
 if __name__ == "__main__":
-    r = requests.post(argv[1], data={'email': argv[2]})
-    print(r.text)
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
